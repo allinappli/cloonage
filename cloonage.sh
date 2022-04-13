9d03635a6420a9218191bfeab54a5e431fd3207f
root <root@f8e4050.online-server.cloud>
2022-04-13 15:01:11 +0200

    first commit

---
+++ cloonage.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+##
+## Développer par Renaud Fradin est Fawzy Elsam
+##
+################################################
+##
+##  Clonage site
+##
+################################################
+
+
+source functions1.sh
+
+echo -e '\e[93m=============================================\033[0m'
+echo "Choisissez l'instance que vous vouler clonner ?"
+echo "Liste des domaines disponibles : "
+echo " "
+listSite=$(plesk bin site --list);
+echo ${listSite[@]}
+echo " "
+read folder_clone
+echo -e '\e[93m=============================================\033[0m'
+
+echo -e '\e[93m=============================================\033[0m'
+echo "Choisissez ou cette instance doit etre cloner ?"
+echo "Liste des domaines disponibles : "
+echo " "
+listSite=$(plesk bin site --list);
+echo ${listSite[@]}
+echo " "
+read folder_destination
+echo -e '\e[93m=============================================\033[0m'
+
+cd $folder_clone/httpdocs
+zip -r $folder_clone.zip *
+cd ../..
+cd $folder_destination/httpdocs
+
+[[ "$(ls -A /)" ]] && deleteContente || mouveContent
+
+mv $folder_clone/httpdocs/$folder_clone.zip $folder_destination/httpdocs
+
+cd $folder_destination/httpdocs
+unzip $folder_clone.zip
+[[ ! -d "index.html" ]] && rm -rf index.html
+rm -rf $folder_clone.zip
---
+++ functions1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+deleteContente(){
+    cd ..
+    pwd
+    rm -rf httpdocs/*
+    cd ..
+}
+
+mouveContent(){
+    cd ../..
+}
