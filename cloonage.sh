7c32b9b2c29b20157a938f018dd6d63ab778bf80
renaud <dev@all-in-appli.com>
2022-04-27 11:50:17 +0200

    fixe pssword

--- cloonage.sh
+++ cloonage.sh
@@ -32,6 +32,11 @@ read mysql_clone_user
 echo -e '\e[93m=============================================\033[0m'
 
 echo -e '\e[93m=============================================\033[0m'
+echo "MDP de l'utilisateur de l'instance à cloner ?"
+read mysql_clone_mdp
+echo -e '\e[93m=============================================\033[0m'
+
+echo -e '\e[93m=============================================\033[0m'
 echo "Choisissez où cette instance doit être clonée ?"
 echo "Liste des domaines disponibles : "
 echo " "
@@ -58,7 +63,7 @@ echo -e '\e[93m=============================================\033[0m'
 
 
 cd $folder_clone/httpdocs
-sudo mysqldump -u $mysql_clone_user -p $mysql_clone_database > $folder_clone.sql
+sudo mysqldump -u $mysql_clone_user -p$mysql_clone_mdp -D $mysql_clone_database > $folder_clone.sql
 sed 's/\sDEFINER=`[^`]*`@`[^`]*`//g' -i $folder_clone.sql
 cd ..
 zip -r $folder_clone.zip httpdocs/
@@ -79,7 +84,7 @@ cd ..
 mv httpdocs/httpdocs/* httpdocs
 cd httpdocs
 
-sudo mysql -u $mysql_destination_user -p $mysql_destination_database < $folder_clone.sql
+sudo mysql -u $mysql_destination_user -p$mysql_destination_mdp  -D $mysql_destination_database < $folder_clone.sql
 [[ ! -d "index.html" ]] && rm -rf index.html
 rm -rf $folder_clone.zip
 rm -rf $folder_clone.sql
@@ -89,8 +94,8 @@ rm -rf httpdocs
 # Modify website url
 echo -e '\e[93m================================================\033[0m'
 echo "Installation DBSearchReplace"
-wget https://github.com/DvdGiessen/DBSR/releases/download/2.2.0/DBSearchReplace-CLI.php
-touch config.json
+[[ `wget -S --spider https://github.com/DvdGiessen/DBSR/releases/download/2.2.0/DBSearchReplace-CLI.php  2>&1 | grep 'HTTP/1.1 200 OK'` ]] && wget https://github.com/DvdGiessen/DBSR/releases/download/2.2.0/DBSearchReplace-CLI.php || exit 1
+[[ ! -d "config.json" ]] && echo "" || rm touch config.json
 echo '{ "options": { "CLI": { "output": "json" }, "PDO": {"hostname": "localhost", "port": 3306, "username": "'$mysql_destination_user'", "password": "'$mysql_destination_mdp'", "database": "'$mysql_destination_database'", "charset": "utf8" }, "DBSR": { "case-insensitive": false, "extensive-search": false, "search-page-size": 10000, "var-match-strict": true, "floats-precision": 5, "convert-charsets": true, "var-cast-replace": true, "db-write-changes": true, "handle-serialize": true } }, "search": ["https://'$folder_clone'"], "replace": ["https://'$folder_destination'"] }' | jq '.' > config.json
 echo "Ouvrir ce lien afin de modifier les datas :" https://$folder_destination/DBSearchReplace-CLI.php?args=--file+config.json
 echo -e '\e[93m================================================\033[0m'
